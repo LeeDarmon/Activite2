23832c131d31822e3d7ee3bed92be0785428bf04
Gabriel Hardy <60385281+HAPPY-STEVE@users.noreply.github.com>
2022-03-23 18:19:30 +0100

    Squelette Test + pom

--- src/test/java/TestProgram.java
+++ src/test/java/TestProgram.java
@@ -1,2 +1,23 @@
+import org.junit.jupiter.api.Test;
+
 public class TestProgram {
+    @Test
+    void getDimension() {
+    }
+
+    @Test
+    void setValue() {
+    }
+
+    @Test
+    void getValue() {
+    }
+
+    @Test
+    void complete() {
+    }
+
+    @Test
+    void possible() {
+    }
 }
