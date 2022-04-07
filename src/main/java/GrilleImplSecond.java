52c82ac36decd05633d8c32773da3fe255b506b5
LeeDarmon <leemickael.darmon@outlook.fr>
2022-04-07 16:27:16 +0200

    Update GrilleImplSecond.java

--- src/main/java/GrilleImplSecond.java
+++ src/main/java/GrilleImplSecond.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class GrilleImplSecond implements Grille{
+public class GrilleImplSecond implements Grille {
 
     public char[][] grille;
 
@@ -32,28 +32,28 @@ public class GrilleImplSecond implements Grille{
 
         List<Character> possibleChar = createListPossible();
 
-        if(checkChar(value, possibleChar)){
+        if(checkChar(value, possibleChar)) {
 
         }
-        else{
+        else {
             return false;
         }
         return false;
     }
 
     // etape 1 : création liste des caractères possibles
-    public List<Character> createListPossible(){
+    public List<Character> createListPossible() {
         //creation tableau possibilités
         int dimension = getDimension();
         List<Character> possibleChar = new ArrayList();
 
-        if(dimension == 9){
-            for(int i = 0; i<possible.length - 6; i++){
+        if(dimension == 9) {
+            for(int i = 0; i<possible.length - 6; i++) {
                 possibleChar.add(possible[i]);
             }
         }
         else{
-            for(int i = 0; i<possible.length; i++){
+            for (int i = 0; i<possible.length; i++) {
                 possibleChar.add(possible[i]);
             }
         }
@@ -62,7 +62,7 @@ public class GrilleImplSecond implements Grille{
 
     // etape 2 : vérification que le caractère est possible
     public boolean checkChar(char value, List<Character> possibleChar)throws IllegalArgumentException {
-        if(possibleChar.contains(value)){
+        if(possibleChar.contains(value)) {
             return true;
         }
         else{
@@ -73,7 +73,7 @@ public class GrilleImplSecond implements Grille{
     //vérification en x que le caractère est possible
     public boolean checkX(int x, char value) throws IllegalArgumentException {
         List<Character> impossibleChar = new ArrayList();
-       for(int i=0; i< grille.length; i++){
+       for(int i=0; i< grille.length; i++) {
            impossibleChar.add(getValue(x,i));
        }
        if(impossibleChar.contains(value)){
@@ -90,7 +90,7 @@ public class GrilleImplSecond implements Grille{
         for(int i=0; i< grille.length; i++){
             impossibleChar.add(getValue(i,y));
         }
-        if(impossibleChar.contains(value)){
+        if(impossibleChar.contains(value)) {
             return false;
         }
         else{
@@ -100,109 +100,93 @@ public class GrilleImplSecond implements Grille{
 
     // vérification du bloc que le caractère est possible
     public boolean checkBloc(int x, int y, char value) throws IllegalArgumentException {
-
         List<Character> impossibleChar = new ArrayList();
         int dimension = getDimension();
-
         int c = 0;
-
         if(dimension == 9){
-            if(x+1 <= 3){
+            if(x+1 <= 3) {
                 c = 3;
-                if(y+1 <=3){
-                    for(int i=0; i<= c ; i++){
+                if(y+1 <=3) {
+                    for(int i=0; i<= c ; i++) {
                         char v1 = getValue(i,0);
                         char v2 = getValue(i,1);
                         char v3 = getValue(i,2);
-
                         Collections.addAll(impossibleChar, v1, v2, v3);
                     }
                 }
-                else if(y+1 <= 6){
-                    for(int i=0; i<= c  ; i++){
+                else if(y+1 <= 6) {
+                    for(int i=0; i<= c  ; i++) {
                         char v1 = getValue(i,3);
                         char v2 = getValue(i,4);
                         char v3 = getValue(i,5);
-
                         Collections.addAll(impossibleChar, v1, v2, v3);
                     }
                 }
                 else if(y+1 <= 9){
-                    for(int i=0; i<=c  ; i++){
+                    for(int i=0; i<=c  ; i++) {
                         char v1 = getValue(i,6);
                         char v2 = getValue(i,7);
                         char v3 = getValue(i,8);
-
                         Collections.addAll(impossibleChar, v1, v2, v3);
                     }
                 }
             }
-            else if(x+1 <= 6){
+            else if(x+1 <= 6) {
                 c = 6;
                 if(y+1 <=3){
-                    for(int i=3; i<= c ; i++){
+                    for(int i=3; i<= c ; i++) {
                         char v1 = getValue(i,0);
                         char v2 = getValue(i,1);
                         char v3 = getValue(i,2);
-
                         Collections.addAll(impossibleChar, v1, v2, v3);
                     }
                 }
-                else if(y+1 <= 6){
+                else if(y+1 <= 6) {
                     for(int i=3; i<= c  ; i++){
                         char v1 = getValue(i,3);
                         char v2 = getValue(i,4);
                         char v3 = getValue(i,5);
-
                         Collections.addAll(impossibleChar, v1, v2, v3);
                     }
                 }
-                else if(y+1 <= 9){
+                else if(y+1 <= 9) {
                     for(int i=3; i<=c  ; i++){
                         char v1 = getValue(i,6);
                         char v2 = getValue(i,7);
                         char v3 = getValue(i,8);
-
                         Collections.addAll(impossibleChar, v1, v2, v3);
                     }
                 }
             }
-            else if(x+1 <= 9){
+            else if(x+1 <= 9) {
                 c = 9;
-                if(y+1 <=3){
-                    for(int i=6; i<= c ; i++){
+                if(y+1 <=3) {
+                    for(int i=6; i<= c ; i++) {
                         char v1 = getValue(i,0);
                         char v2 = getValue(i,1);
                         char v3 = getValue(i,2);
-
                         Collections.addAll(impossibleChar, v1, v2, v3);
                     }
                 }
-                else if(y+1 <= 6){
-                    for(int i=6; i<= c  ; i++){
+                else if(y+1 <= 6) {
+                    for(int i=6; i<= c  ; i++) {
                         char v1 = getValue(i,3);
                         char v2 = getValue(i,4);
                         char v3 = getValue(i,5);
-
                         Collections.addAll(impossibleChar, v1, v2, v3);
                     }
                 }
-                else if(y+1 <= 9){
-                    for(int i=6; i<=c  ; i++){
+                else if(y+1 <= 9) {
+                    for(int i=6; i<=c  ; i++) {
                         char v1 = getValue(i,6);
                         char v2 = getValue(i,7);
                         char v3 = getValue(i,8);
-
                         Collections.addAll(impossibleChar, v1, v2, v3);
                     }
                 }
             }
         }
-
-
-
         System.out.println(impossibleChar);
-
         if(impossibleChar.contains(value)){
             return false;
         }
