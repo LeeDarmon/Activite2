0b81044c62aee20dda61ad1073f3887324c8c81d
LeeDarmon <leemickael.darmon@outlook.fr>
2022-04-07 16:37:23 +0200

    Update GrilleImplSecond.java

--- src/main/java/GrilleImplSecond.java
+++ src/main/java/GrilleImplSecond.java
@@ -32,7 +32,7 @@ public class GrilleImplSecond implements Grille {
 
         List<Character> possibleChar = createListPossible();
 
-        if(checkChar(value, possibleChar)) {
+        if (checkChar(value, possibleChar)) {
 
         }
         else {
@@ -47,12 +47,12 @@ public class GrilleImplSecond implements Grille {
         int dimension = getDimension();
         List<Character> possibleChar = new ArrayList();
 
-        if(dimension == 9) {
-            for(int i = 0; i<possible.length - 6; i++) {
+        if (dimension == 9) {
+            for (int i = 0; i<possible.length - 6; i++) {
                 possibleChar.add(possible[i]);
             }
         }
-        else{
+        else {
             for (int i = 0; i<possible.length; i++) {
                 possibleChar.add(possible[i]);
             }
@@ -62,10 +62,10 @@ public class GrilleImplSecond implements Grille {
 
     // etape 2 : vérification que le caractère est possible
     public boolean checkChar(char value, List<Character> possibleChar)throws IllegalArgumentException {
-        if(possibleChar.contains(value)) {
+        if (possibleChar.contains(value)) {
             return true;
         }
-        else{
+        else {
             return false;
         }
     }
@@ -73,13 +73,13 @@ public class GrilleImplSecond implements Grille {
     //vérification en x que le caractère est possible
     public boolean checkX(int x, char value) throws IllegalArgumentException {
         List<Character> impossibleChar = new ArrayList();
-       for(int i=0; i< grille.length; i++) {
+       for (int i=0; i< grille.length; i++) {
            impossibleChar.add(getValue(x,i));
        }
-       if(impossibleChar.contains(value)){
+       if (impossibleChar.contains(value)) {
            return false;
        }
-       else{
+       else {
            return true;
        }
     }
@@ -87,10 +87,10 @@ public class GrilleImplSecond implements Grille {
     // vérification en y que le caractère est possible
     public boolean checkY(int y, char value) throws IllegalArgumentException {
         List<Character> impossibleChar = new ArrayList();
-        for(int i=0; i< grille.length; i++){
+        for (int i=0; i< grille.length; i++) {
             impossibleChar.add(getValue(i,y));
         }
-        if(impossibleChar.contains(value)) {
+        if (impossibleChar.contains(value)) {
             return false;
         }
         else{
@@ -103,27 +103,27 @@ public class GrilleImplSecond implements Grille {
         List<Character> impossibleChar = new ArrayList();
         int dimension = getDimension();
         int c = 0;
-        if(dimension == 9){
-            if(x+1 <= 3) {
+        if (dimension == 9) {
+            if (x+1 <= 3) {
                 c = 3;
-                if(y+1 <=3) {
-                    for(int i=0; i<= c ; i++) {
+                if (y+1 <=3) {
+                    for (int i=0; i<= c ; i++) {
                         char v1 = getValue(i,0);
                         char v2 = getValue(i,1);
                         char v3 = getValue(i,2);
                         Collections.addAll(impossibleChar, v1, v2, v3);
                     }
                 }
-                else if(y+1 <= 6) {
-                    for(int i=0; i<= c  ; i++) {
+                else if (y+1 <= 6) {
+                    for (int i=0; i<= c  ; i++) {
                         char v1 = getValue(i,3);
                         char v2 = getValue(i,4);
                         char v3 = getValue(i,5);
                         Collections.addAll(impossibleChar, v1, v2, v3);
                     }
                 }
-                else if(y+1 <= 9){
-                    for(int i=0; i<=c  ; i++) {
+                else if (y+1 <= 9){
+                    for (int i=0; i<=c  ; i++) {
                         char v1 = getValue(i,6);
                         char v2 = getValue(i,7);
                         char v3 = getValue(i,8);
@@ -131,26 +131,26 @@ public class GrilleImplSecond implements Grille {
                     }
                 }
             }
-            else if(x+1 <= 6) {
+            else if (x+1 <= 6) {
                 c = 6;
-                if(y+1 <=3){
-                    for(int i=3; i<= c ; i++) {
+                if (y+1 <=3) {
+                    for (int i=3; i<= c ; i++) {
                         char v1 = getValue(i,0);
                         char v2 = getValue(i,1);
                         char v3 = getValue(i,2);
                         Collections.addAll(impossibleChar, v1, v2, v3);
                     }
                 }
-                else if(y+1 <= 6) {
-                    for(int i=3; i<= c  ; i++){
+                else if (y+1 <= 6) {
+                    for (int i=3; i<= c  ; i++) {
                         char v1 = getValue(i,3);
                         char v2 = getValue(i,4);
                         char v3 = getValue(i,5);
                         Collections.addAll(impossibleChar, v1, v2, v3);
                     }
                 }
-                else if(y+1 <= 9) {
-                    for(int i=3; i<=c  ; i++){
+                else if (y+1 <= 9) {
+                    for (int i=3; i<=c  ; i++) {
                         char v1 = getValue(i,6);
                         char v2 = getValue(i,7);
                         char v3 = getValue(i,8);
@@ -158,9 +158,9 @@ public class GrilleImplSecond implements Grille {
                     }
                 }
             }
-            else if(x+1 <= 9) {
+            else if (x+1 <= 9) {
                 c = 9;
-                if(y+1 <=3) {
+                if (y+1 <=3) {
                     for(int i=6; i<= c ; i++) {
                         char v1 = getValue(i,0);
                         char v2 = getValue(i,1);
@@ -168,7 +168,7 @@ public class GrilleImplSecond implements Grille {
                         Collections.addAll(impossibleChar, v1, v2, v3);
                     }
                 }
-                else if(y+1 <= 6) {
+                else if (y+1 <= 6) {
                     for(int i=6; i<= c  ; i++) {
                         char v1 = getValue(i,3);
                         char v2 = getValue(i,4);
@@ -176,8 +176,8 @@ public class GrilleImplSecond implements Grille {
                         Collections.addAll(impossibleChar, v1, v2, v3);
                     }
                 }
-                else if(y+1 <= 9) {
-                    for(int i=6; i<=c  ; i++) {
+                else if (y+1 <= 9) {
+                    for (int i=6; i<=c  ; i++) {
                         char v1 = getValue(i,6);
                         char v2 = getValue(i,7);
                         char v3 = getValue(i,8);
@@ -187,10 +187,10 @@ public class GrilleImplSecond implements Grille {
             }
         }
         System.out.println(impossibleChar);
-        if(impossibleChar.contains(value)){
+        if (impossibleChar.contains(value)) {
             return false;
         }
-        else{
+        else {
             return true;
         }
 
